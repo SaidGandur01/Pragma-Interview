d11cc2f9f4a4cc353f452c5557b28b1a8a5ce5fc
GUARUMO <guarumo@GUARUMOs-MacBook-Pro.local>
2020-10-23 20:14:01 -0500

    new_Changes

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -19,16 +19,37 @@ export class RegistrationComponent {
     this.arrayDates = this.dates.split("\n");
     console.log("Array nombres: ", this.arrayNames);
     console.log("Array Fechas: ", this.arrayDates);
-    this.bandera = true;
 
-    for (let i = 0; i < this.arrayDates.length; i++){
-      console.log(this.arrayDates[i]);
-      let fecha_izquierda = this.arrayDates[i].split(" to ");
-      console.log("Fecha izquierad: ", fecha_izquierda);
-      for (let j = fecha_izquierda[0].substr(9, 10); j <= fecha_izquierda[1].substr(9, 10); j++){
-        this.items.push(fecha_izquierda[0].substr(0, 9) + j);
-        console.log("Estos son los items: ", this.items);
+    if (this.arrayDates.length != this.arrayNames.length) {
+      this.bandera = false;
+      alert("Número de objetos en columnas no concuerda, por favor asegurarse que sea igual");
+    } else {
+      for (let i = 0; i < this.arrayDates.length; i++) {
+        console.log(this.arrayDates[i]);
+        let fecha_izquierda = this.arrayDates[i].split(" to ");
+        console.log("Fecha izquierad: ", fecha_izquierda);
+
+        if (this.dateValidator(fecha_izquierda[0]) && this.dateValidator(fecha_izquierda[1])) {
+          this.bandera = true;
+          for (let j = fecha_izquierda[0].substr(9, 10); j <= fecha_izquierda[1].substr(9, 10); j++) {
+            this.items.push(fecha_izquierda[0].substr(0, 9) + j);
+            console.log("Estos son los items: ", this.items);
+          }
+        } else {
+          this.bandera = false;
+          alert("Formato de fechas incorrecto, por favor corregirlo");
+        }   
       }
+    }
+  }
+
+  public dateValidator(fecha) {
+    
+    let validator = /^\d{4}\-\d{2}\-\d{2}$/;
+    if ((fecha.match(validator)) && (fecha != '')) {
+      return true;
+    } else {
+      return false;
     }
   }
 }
