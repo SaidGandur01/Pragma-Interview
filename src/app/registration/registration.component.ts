bb89c60be01280b5bec20c8878decd3d8d9eec8a
GUARUMO <guarumo@GUARUMOs-MacBook-Pro.local>
2020-10-26 15:14:08 -0500

    Said Gandur Interview

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -5,8 +5,23 @@ import { Component } from '@angular/core';
   templateUrl: './registration.component.html'
 })
 export class RegistrationComponent {
+
+  /*
+  Buenas tardes, el código recibe items de dos campos, primero que todo hice las validaciones para que los campos tuviera exactamente el mismo tamaño, que era una de las condiciones del ejercicio, posteriormente la validación del formato de las fechas que era otra de las condiciones del ejercicio. 
+
+  Una vez hecho esto lo que hice es tratar la información para crear un array con el y/o nombres(En caso de que sean mas de uno), y fechas. 
+
+  En el html, usamos tres directivas de angular principales, el ngModel para tomar los datos dinámicamente de los campos y asignarselos a las variables, usamos la directiva ng if para asegurarnos de generar una lista con toda la data una vez las validaciones no generen ningún erro, y por ultimo usamos la directiva ng for que nos permite iterar en un array de elementos para imprimir su información. 
+      
+  En este ejercicio obtengo dos array, uno con los nombres y otro con las fechas, cada fecha asociada a un nombre, en el html pongo a iterar los nombres primero y por cada nombre obtengo las fechas pertinentes, una vez termina de iterar la fecha, continua con el siguiente nombre y sus respectivas fechas sucesivamente hasta que la data termina. 
+    
+  Agradezco por la revisión del archivo y sobretodo el tiempo tomado para revisar el código.
+  */
+
   bandera: boolean = false;
   arrayNames = [];
+  arrayNamesNew = [];
+  arrayDatesNew = [];
   arrayDates = [];
   names: string = "";
   dates: string = "";
@@ -15,6 +30,8 @@ export class RegistrationComponent {
 
   generateList() {
     this.items = [];
+    this.arrayNamesNew = [];
+    this.arrayDatesNew = [];
     this.arrayNames = this.names.split("\n");
     this.arrayDates = this.dates.split("\n");
     console.log("Array nombres: ", this.arrayNames);
@@ -25,16 +42,16 @@ export class RegistrationComponent {
       alert("Número de objetos en columnas no concuerda, por favor asegurarse que sea igual");
     } else {
       for (let i = 0; i < this.arrayDates.length; i++) {
+        this.items = [];
         console.log(this.arrayDates[i]);
         let fecha_izquierda = this.arrayDates[i].split(" to ");
-        console.log("Fecha izquierad: ", fecha_izquierda);
-
         if (this.dateValidator(fecha_izquierda[0]) && this.dateValidator(fecha_izquierda[1])) {
           this.bandera = true;
-          for (let j = fecha_izquierda[0].substr(9, 10); j <= fecha_izquierda[1].substr(9, 10); j++) {
-            this.items.push(fecha_izquierda[0].substr(0, 9) + j);
-            console.log("Estos son los items: ", this.items);
+          for (let j = parseInt(fecha_izquierda[0].substr(8, 2).trimLeft('0')); j <= parseInt(fecha_izquierda[1].substr(8, 2).trimLeft('0')); j++) {
+            this.items.push(fecha_izquierda[0].substr(0, 8) + j);
           }
+          this.arrayNamesNew.push(this.arrayNames[i]);
+          this.arrayDatesNew.push(this.items);
         } else {
           this.bandera = false;
           alert("Formato de fechas incorrecto, por favor corregirlo");
